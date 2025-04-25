b4e4aaca178d514ec5273acc672e8bc4ce61f73e
hrit99 <hritikpihal51299@gmail.com>
2025-04-25 19:38:42 +0530

    Added rbac 2

--- app/phone-numbers/phone-number-page.tsx
+++ app/phone-numbers/phone-number-page.tsx
@@ -91,6 +91,7 @@ export default function PhoneNumbersPage() {
           </PermissionWrapper>
         </div>
 
+
         <div className="flex flex-wrap items-center justify-between gap-4">
           <div className="relative flex-1 max-w-md">
             <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 h-4 w-4 text-gray-400" />
